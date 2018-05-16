b27e41b1ce65add34187dbcf36d7eea465075ffa
alen <wuning1990@163.com>
2018-05-16 22:52:38 +0800

    no message

--- PIKAuthor/ViewController.m
+++ PIKAuthor/ViewController.m
@@ -1,16 +1,14 @@
 //
 //  ViewController.m
-//  WNImagePicker
+//  PIKAuthor
 //
-//  Created by wuning on 16/5/10.
-//  Copyright © 2016年 alen. All rights reserved.
+//  Created by ningwu on 2018/5/16.
+//  Copyright © 2018年 alen. All rights reserved.
 //
 
 #import "ViewController.h"
-#import "WNImagePicker.h"
-#import "WNImapgePicker/ImageEditVC.h"
 
-@interface ViewController ()<WNImagePickerDelegate>
+@interface ViewController ()
 
 @end
 
@@ -21,29 +19,11 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)onCamare:(id)sender {
-    WNImagePicker *pickerVC  = [[WNImagePicker alloc]init];
-    pickerVC.delegate = self;
-    UINavigationController *navVC = [[UINavigationController alloc]initWithRootViewController:pickerVC];
-    [self presentViewController:navVC animated:YES completion:nil];
-}
 
-#pragma mark -- WNImagePickerDelegate
-- (void)getCutImage:(UIImage *)image controller:(WNImagePicker *)vc
-{
-    [vc.navigationController dismissViewControllerAnimated:NO completion:nil];
-    ImageEditVC *editVC = [[ImageEditVC alloc]init];
-    editVC.image = image;
-    [self.navigationController pushViewController:editVC animated:YES];
-}
-
-- (void)onCancel:(WNImagePicker *)vc
-{
-    [vc.navigationController dismissViewControllerAnimated:YES completion:nil];
-}
 @end
